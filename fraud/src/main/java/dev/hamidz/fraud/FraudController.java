abea5331c6572a0bed1317c10ca9672815441a36
Hamid Ali Kalhoro <hamidKalhoro.in@gmail.com>
2025-06-03 13:33:31 +0500

    inter-connecting customer and fraud services

--- fraud/src/main/java/dev/hamidz/fraud/FraudController.java
+++ fraud/src/main/java/dev/hamidz/fraud/FraudController.java
@@ -1,11 +1,13 @@
 package dev.hamidz.fraud;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @RequiredArgsConstructor
+@Slf4j
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
@@ -13,7 +15,9 @@ public class FraudController {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(
             @PathVariable("customerId") Integer customerId) {
-        boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        boolean isFraudulentCustomer = fraudCheckService
+                .isFraudulentCustomer(customerId);
+        log.info("fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
